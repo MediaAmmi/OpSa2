d05227042d3f07b53d59670dcb78ce44e9ba9bf3
media <media@DESKTOP-7BPJ38H>
2020-11-01 12:40:40 +0100

    first step

--- Basisanwendung/src/gui/Main.java
+++ Basisanwendung/src/gui/Main.java
@@ -7,13 +7,6 @@ public class Main {
 
 		System.out.println("Hi Media");
 		
-		System.out.println("Wie gehts dir");
-		System.out.println("Was machst du media");
-		System.out.println("I am sad");
-		System.out.println("Wie gehts dir");
-		System.out.println("Was machst du media");
-		System.out.println("I am sad");
-		
 
 		
 	}
